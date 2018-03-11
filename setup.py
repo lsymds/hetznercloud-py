31d3fd3d87a4628b4d87e3d3048dc3c884d4bfdb
Liam Symonds <liam@ls-software.uk>
2018-03-11 19:18:00 +0000

    Add deployment to Pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="hetznercloud",
+    packages=["hetznercloud"],
+    version="1.0.0",
+    description="Hetzner Cloud SDK",
+    author="Liam Symonds",
+    author_email="liam@ls-software.uk",
+    url="https://github.com/elsyms/hetznercloud-py",
+    keywords=["hetzner", "hetznercloud", "hetzner cloud api", "hetzner sdk", "hetzner api"],
+    classifiers=[]
+)
